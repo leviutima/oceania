bc090a24b36bd800551eead4aba20134541e1467
Levi_Utima <146796839+leviutima@users.noreply.github.com>
2024-06-07 21:04:31 -0300

    adicionando textos e responsividade a pagina sobre-nós

--- src/app/Sobre/page.tsx
+++ src/app/Sobre/page.tsx
@@ -39,19 +39,44 @@ const Sobre = () => {
                         </a>
                 </div>
                 <section id="vivian" className={style.vivian}>
-                    <div>
-                       <h2>Vivian</h2>
-                        <p>Trabalhando por de baixo dos panos, Vivian é responsável pelas documentações da plataforma, visando a organização 
-                            e padronização dos detalhes trabalhando também com a analises de dados da Ocêania.</p> 
+                    <div className={style.containerVivian}>
+                        <div >
+                        <h2>Vivian</h2>
+                            <p>Trabalhando por de baixo dos panos, Vivian é responsável pelas documentações da plataforma, visando a organização 
+                                e padronização dos detalhes trabalhando também com a analises de dados da Ocêania. Formada em Audiovisual pelo Senac, 
+                                trabalhou por 4 anos com edição de vídeo e animação, mas sempre se interessou muito por tecnologia e tentava se manter 
+                                atualizada nas notícias dessa área. Em 2020, conseguiu um emprego em uma multinacional, onde trabalhou na área de infraestrutura 
+                                de TI e teve contato com desenvolvedores de software, o que despertou seu interesse, e em 2023 decidiu finalmente se matricular 
+                                na FIAP, no curso de Análise e Desenvolvimento de Sistemas, que nos trouxe até este momento.
+                                </p>
+                        </div>
+                            <Image src={Vivian} alt="foto da vivian" width={300}/>
                     </div>
-                    <Image src={Vivian} alt="foto da vivian"/>
                 </section>
-                <section id="joji">
-                        <h2>Jhonatan</h2>
-                        <p>Trabalhando também por de baixo dos panos, Jhonatan é responsavel pelo back-end da plataforma, trabalhando com 
-                        toda lógica de armazenamento de dados necessários para o funcionamento da plataforma.</p>
+                <section id="joji" className={style.joji}>
+                    <div className={style.containerJoji}>
+                        <Image src={Joji} alt="foto do Jhonatan" width={300}/>
+                        <div className={style.textJoji}>
+                            <h2>Jhonatan</h2>
+                            <p>Trabalhando também por de baixo dos panos, Jhonatan é responsavel pelo back-end da plataforma, trabalhando com 
+                            toda lógica de armazenamento de dados necessários para o funcionamento da plataforma. Estudante do segundo semestre 
+                            de Análise e Desenvolvimento de Sistemas. Proficiente em backend e banco de dados. Como hobby, gosta de estudar, jogar 
+                            videogame e fotografar. Atualmente, está fissurado no aprendizado de VBA</p>
+                        </div>
+                    </div>
                 </section>
-                <section id="levi">Levi</section>
+                <section id="levi" className={style.levi}>
+                    <div className={style.containerLevi}>
+                        <div className={style.textLevi}>
+                            <h2>Levi</h2>
+                            <p>Trabalhando na parte visual, Levi ficou responsável pela parte da criação do site da oceânia, trabalhando com UX e 
+                            integração do back-end com o front-end. Levi é um estudante de Análise e Desenvolvimento de Sistemas na Faculdade FIAP.
+                            No momento está voltado mais ao front-end para se especializar, mas é apaixonado pelo back-end. Nos momentos livres gosta de jogar,
+                            fazer exercícios físicos e assistir um bom filme.</p>
+                        </div>
+                        <Image src={Levi} alt="Uma foto do levi" width={300}/>
+                    </div>
+                </section>
             </section>
         </>
     )
